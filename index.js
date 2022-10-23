2e9aea8faf9a0da2aa6293f771ee421453428cfe
Joel Isaias Solano Ocampo <joel.solano59@gmail.com>
2022-10-23 16:33:28 -0500

    ReactDOM.render()
    Learned how to use ReactDOM.render on index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//ReactDOM.render(<h1>Hi, everyone!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+  </ul>,
+  document.getElementById("root")
+)
